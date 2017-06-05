0aefa5fc6dc80b576ab5c69b2934f256dfc8b9de
Adi Sayoga <adisayoga@gmail.com>
2017-06-05 13:46:11 +0800

    Tambah kerangka route.

--- routes.js
+++ routes.js
@@ -1,8 +1,61 @@
 module.exports = function(app) {
 
-  // GET /
+  /*
+   * GET /
+   */
   app.get('/', function(req, res) {
+    res.redirect('/blog');
+  });
+
+  /*
+   * GET /blog
+   */
+  app.get('/blog', function(req, res) {
     res.render('index');
+  });
+
+  /*
+   * GET /blog/:id
+   */
+  app.get('/blog/:id', function(req, res) {
+
+  });
+
+  // ------------------------------
+
+  /*
+   * GET /blog/create
+   */
+  app.get('/blog/create', function(req, res) {
+
+  });
+
+  /*
+   * POST /blog/create
+   */
+  app.post('/blog/create', function(req, res) {
+
+  });
+
+  /*
+   * GET /blog/update/:id
+   */
+  app.get('/blog/update/:id', function(req, res) {
+
+  });
+
+  /*
+   * PUT /blog/update/:id
+   */
+  app.put('/blog/update/:id', function(req, res) {
+
+  });
+
+  /*
+   * DELETE /blog/delete
+   */
+  app.delete('/blog/delete', function(req, res) {
+
   });
 
 };
