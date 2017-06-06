d8411698eaee53d86dcc67b7725206a7e5f3ddc6
Adi Sayoga <adisayoga@gmail.com>
2017-06-06 09:53:00 +0800

    Pindah dari /src ke /app.

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ db.once('open', function() {
   console.info('Database connected');
 });
 
-require('./src/routes')(app);
+require('./app/routes')(app);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function(err) {
